264bfb5ed2b815d85e4c19c7280fa132ad7abeda
amb2112-data <ambrosio2112.data@gmail.com>
2023-09-21 11:52:58 -0500

    First running version 0.0.1

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
-print("hello world!")
+import sys
+import os
+from PIL import Image, ImageFilter
+
+source_folder = sys.argv[1]
+output_folder = sys.argv[2]
+
+source_path = f'.{source_folder}'
+output_path = f'.{output_folder}'
+
+print(os.path.exists(source_path))
+print(os.path.exists(output_path))
+
+input_imgs_list = os.listdir(source_path)
+
+print(input_imgs_list)
+
+for i in input_imgs_list:
+    i_split = os.path.splitext(i)[0]
+    i_path = f'{source_path}/{i}'
+    with Image.open(i_path) as im:
+        im_dt = im.filter(ImageFilter.DETAIL)
+        im_bw = im.convert('L')
+        im_bw.save(f'{output_path}/{i_split}.png', "png")
+
